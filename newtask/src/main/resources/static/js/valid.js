c5adee7e96fb465efde442cea5044cda79d10e13
razbowoo <kirya.kuzmich@gmail.com>
2018-08-13 14:58:30 +0300

    work with validation form on js

--- newtask/src/main/resources/static/js/valid.js
+++ newtask/src/main/resources/static/js/valid.js
@@ -1,52 +1,81 @@
 // var form = document.querySelector('.')
-var fields = document.querySelectorAll('.form-control')
-var form = document.getElementById('registration')
-var password = form.querySelector('.password')
-var confirmPassword = form.querySelector('.confirmPassword')
-var btn=document.getElementById('btn')
+var fields = document.querySelectorAll('.form-control');
+var form = document.getElementById('registration');
+var password = form.querySelector('.password');
+var confirmPassword = form.querySelector('.confirmPassword');
+var btn = document.getElementById('btn');
+var email = document.getElementById('email');
+var isErrors = false;
+
 
 var generateError = function (text) {
-    var error = document.createElement('div')
-    error.className = 'error'
-    error.style.color = 'red'
-    error.innerHTML = text
-    return error
+    isErrors = false;
+    var error = document.createElement('div');
+    error.className = 'error';
+    error.style.color = 'red';
+    error.innerHTML = text;
+    isErrors = true;
+    return error;
 }
 
 var removeValidation = function () {
-    var errors = document.querySelectorAll('.error')
+    var errors = document.querySelectorAll('.error');
 
     for (var i = 0; i < errors.length; i++) {
-        errors[i].remove()
+        errors[i].remove();
     }
+
 }
 
 var checkFieldsPresence = function () {
+    var bool = false
     for (var i = 0; i < fields.length; i++) {
         if (!fields[i].value) {
-            console.log('field is blank', fields[i])
-            var error = generateError('Cannot be blank')
+            console.log('field is blank', fields[i]);
+            var error = generateError('Cannot be blank');
             form[i].parentElement.insertBefore(error, fields[i])
-
+            bool = true;
         }
     }
+    return bool
+}
+
+function validateEmail(email) {
+    var bool = false
+    var re = /\S+@\S+\.\S+/;
+    if (email.value !== re.value) {
+        console.log('not equals');
+        console.log(email);
+        var error = generateError('Invalid email');
+        email.parentElement.insertBefore(error, email);
+        bool = true;
+    }
+    return bool
 }
 
 var checkPasswordMatch = function () {
+    var bool = false
     if (password.value !== confirmPassword.value) {
-        console.log('not equals')
-        var error = generateError('Password doesnt match')
-        password.parentElement.insertBefore(error, password)
+        console.log('not equals');
+        console.log(confirmPassword);
+        var error = generateError('Password doesnt match');
+        password.parentElement.insertBefore(error, password);
+        bool = true;
     }
+    return bool
+
 }
 
-form.addEventListener('submit', function (event) {
-    event.preventDefault()
 
-    removeValidation()
+function beforeSubmit() {
+    removeValidation();
 
-    checkFieldsPresence()
 
-    checkPasswordMatch()
-    console.log(1)
-})
+    if (!checkFieldsPresence() && !checkPasswordMatch() && !validateEmail(email) ) {
+        console.log(1);
+        document.getElementById('registration').submit()
+    }
+
+
+}
+
